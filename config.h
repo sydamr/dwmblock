b480a05ed30ffe07779e8218a5a7352a9369af75
Syed Amer <syedaqil001@gmail.ccom>
2021-11-15 17:43:37 +0800

    .

--- config.h
+++ config.h
@@ -2,11 +2,13 @@
 static const Block blocks[] = {
 
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"",  "music",  1, 10},
-  {"",  "uptime.sh", 2,  1},
-  {"",  "temp", 1,  1},
+  {"",  "music",  1, 5},
+  {"⏱",  "uptime.sh", 2,  4},
+  {"",  "weather", 2,  8},
+  {"🌡",  "temp", 1,  3},
+  {"💾",  "ram", 5,  7},
   {"",  "volume", 1,  1},
-  {"",  "clock",  60, 1},
+  {"🗓️",  "clock",  30, 2},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
